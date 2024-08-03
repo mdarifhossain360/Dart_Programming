6bbfe3aa126d2563a45a0c41a1dffb61ffcda5b5
MD Arif Hossain <81848585+mdarifhossain360@users.noreply.github.com>
2024-08-03 12:44:58 +0600

    Update numberGuessingGame.dart

--- numberGuessingGame.dart
+++ numberGuessingGame.dart
@@ -4,9 +4,10 @@ void main(){
   guessNumber();
 }
 void guessNumber(){
-  int score=0;
    Random random = Random();
   while (true) {
+    int score=0;
+    String? option;
     int randomNumber = random.nextInt(5) + 1;
     stderr.write("Enter your number :> ");
     int number=int.parse(stdin.readLineSync()!);
@@ -15,12 +16,24 @@ void guessNumber(){
       print('\tYou gess the number !');
     }
     if(score>10){
-      score=score+9;
+      if(score==20||score==30||score==40||score==50||score==60||score==70||score==80||score==90||score==100){
+         print('Your total score is :> $score');
+        stderr.write('Do you want to continure (yes/y || no/n):>');
+        option = stdin.readLineSync()!.toLowerCase();
+        if (option == 'y' || option == 'yes') {
+          continue;
+        }
+        if (option == 'n' || option == 'no') {
+          break;
+        } else {
+          print('Invalid input! please answer yes/y or no/n.');
+        }
+      }
     }
     if(score==10){
-      print('Your total score is :> $score');
+    print('Your total score is :> $score');
       stderr.write('Do you want to continure (yes/y || no/n):>');
-      String option=stdin.readLineSync()!.toLowerCase();
+      option=stdin.readLineSync()!.toLowerCase();
       if(option=='y'||option=='yes'){
         continue;
       }
